31eb725c021d5d483231c99720a5d8add9020e7a
Jan Tošovský <jan.tosovsky.cz@gmail.com>
2021-02-17 21:54:26 +0100

    Initial commit

---
+++ src/main/java/in/drifted/tools/hellojavafx/Main.java
@@ -0,0 +1,27 @@
+package in.drifted.tools.hellojavafx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button button = new Button();
+        button.setText("Say 'Hello World'");
+        button.setOnAction(event -> System.out.println("Hello World!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(button);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/module-info.java
@@ -0,0 +1,6 @@
+module in.drifted.tools.hellojavafx {
+
+    requires javafx.controls;
+
+    exports in.drifted.tools.hellojavafx;
+}
